e3858f1a8fd6bfd0f243c3ed06fabe3e1afab0dc
etashkinov <gene@gene.co.uk>
2018-07-15 22:21:38 +0100

    Events

--- app/src/main/java/com/ewind/hl/model/event/EventTypeFactory.java
+++ app/src/main/java/com/ewind/hl/model/event/EventTypeFactory.java
@@ -3,10 +3,14 @@ package com.ewind.hl.model.event;
 import android.support.annotation.NonNull;
 
 import com.ewind.hl.model.event.detail.EventDetail;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.util.List;
 
 public class EventTypeFactory {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper(new YAMLFactory());
 
     @NonNull
     public static <T extends EventDetail> EventType<T> get(String typeName) {
